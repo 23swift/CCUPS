dc98d8b8ea881711b32af1a600c3b0d581314f88
Arnold B. Costamero <a012001164@HO.AD.BDO>
2019-07-15 18:34:05 +0800

    "Sorting"

--- src/components/billingInput/HeaderConfig.js
+++ src/components/billingInput/HeaderConfig.js
@@ -13,6 +13,7 @@ import { styled } from '@material-ui/styles';
 import { CircularProgress } from '@material-ui/core';
 import FormDialog from '../common/FormDialog';
 import { blue, grey } from '@material-ui/core/colors';
+import SortableList from './../common/SortableList';
 const CustomProgress=styled(CircularProgress)({
 
  
@@ -54,20 +55,82 @@ export default class  HeaderConfig extends Component {
    
     
   }
+  updateSequenceConfig=(config)=>{
+    // config={...config,fileSection:1,fileType:1}
 
+      fetch('api/UpdateConfigSequence',{
+        method:'PUT',
+        headers:{'content-type':'application/json'},
+        body:JSON.stringify(config)
+        
+    
+    }).then(
+              // console.log(this);
+              // console.log('Add function for Billing Input header:'+ JSON.stringify(config));
+              this.getconfigList()
+        
+    );
+   
+    
+  }
+
+  
   getconfigList=()=>{
     // console.log('code to get loist here');
     this.setState({...this.state,isLoading:true});
-    fetch("/api/GetAllInputFileConfig").then(response => response.json()).
+    fetch("/api/GetAllInputFileConfig").
+    then(response => response.json()).
     then((data)=>{
       setTimeout(()=>{
         this.setState({...this.state,configList:data});  
         this.setState({...this.state,isLoading:false});
-      },2000)
+        console.log(this.state.configList);
+      },1000)
+      // this.setState({...this.state,configList:data});  
+      // this.setState({...this.state,isLoading:false});
+      // console.log(this.state.configList);
         
     });
     
   }
+
+  onListChange=(order,oldIndex,newIndex)=>{
+    let configList=this.state.configList;
+    let list=[]
+    let counter=1;
+    
+    
+      // order.map(orderItem=>{
+      //    var result= this.state.configList.find((e) => e.sequenceNum == orderItem)
+   
+     
+      //    result={...result,sequenceNum:counter}
+      //    list.push(result);
+      //    ++counter;
+      // });
+      // console.log(list);
+   var currentItem=this.state.configList.find((e) => e.sequenceNum == oldIndex);
+  
+   var movedItem=this.state.configList.find((e) => e.sequenceNum == newIndex);
+  
+   currentItem.sequenceNum=newIndex;
+   movedItem.sequenceNum=oldIndex;
+
+   var affectedConfig=[currentItem,movedItem]
+   this.updateSequenceConfig(affectedConfig);
+  
+
+   console.log(currentItem);
+   console.log(movedItem);
+   
+  
+  
+  //     this.setState({...this.state,configList:list});  
+  //     console.log(this.state.configList);
+      
+    
+  }
+
    handleClickOpen=()=> {
     this.setState({...this.state,openDialog:true});
   }
@@ -100,12 +163,17 @@ export default class  HeaderConfig extends Component {
       
            
            
-          {/* <hr/> */}
+          
           {this.state.isLoading && <Box display="flex" flexDirection="row"  flexWrap="nowrap" mx={1} justifyContent="space-between" width={130}>
                                                         <Box ><CustomProgress size={20} /></Box> <Box ><Typography  color="primary">Please wait...</Typography>
                                                         </Box></Box>
                                                       } 
-             <ConfigList title="Header Configuration" items={this.state.configList}/> 
+             <ConfigList title="Header Configuration" items={this.state.configList} onChange={this.onListChange}/>
+
+             {/* <SortableList items={this.state.configList}/> */}
+             
+             {/* <SortableList/> */}
+
              {/* <ConfigTestArea/>
             <MatchInfo/> */}
          
--- src/components/common/ConfigList.js
+++ src/components/common/ConfigList.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+
 import { makeStyles,styled } from '@material-ui/core/styles';
 import { TableCell, TableRow, Paper, Toolbar, Table, TableHead, Fab, TableBody, Typography, MenuItem, Grid, List, ListItem, ListItemText,Avatar, Divider, ListItemAvatar, Badge, Button, IconButton } from '@material-ui/core';
 import { amber, green, blue, grey } from '@material-ui/core/colors';
@@ -7,20 +9,22 @@ import FormDialog from './FormDialog'
 import useStyles from './useStyles';
 import { Edit, HighlightOff, Clear } from '@material-ui/icons';
 import AlertDialog from './AlertDialog';
+import uniqueId from 'lodash/uniqueId';
+
+import ReactSortable  from 'react-sortablejs';
 
 
-
-function createData(sq,fieldName, dataType, size, format, mappToSystem) {
-    return {sq, fieldName, dataType, size, format, mappToSystem };
-  }
+// function createData(sq,fieldName, dataType, size, format, mappToSystem) {
+//     return {sq, fieldName, dataType, size, format, mappToSystem };
+//   }
   
-  const rows = [
-    createData(1,'recordType', 'Numeric', 3, '',  false),
-    createData(2,'cardNumber', 'Numeric', 16, '', false),
-    createData(3,'name', 'Alpha Numeric', 30, '',false),
-    createData(4,'accountNumber', 'Numeric', 10,'', false),
-    createData(5,'amount', 'Decimal', 13, '0000000000000.00', false),
-  ];
+//   const rows = [
+//     createData(1,'recordType', 'Numeric', 3, '',  false),
+//     createData(2,'cardNumber', 'Numeric', 16, '', false),
+//     createData(3,'name', 'Alpha Numeric', 30, '',false),
+//     createData(4,'accountNumber', 'Numeric', 10,'', false),
+//     createData(5,'amount', 'Decimal', 13, '0000000000000.00', false),
+//   ];
 
   const  ConfigList=(props)=> {
     const classes = useStyles();
@@ -38,6 +42,11 @@ function createData(sq,fieldName, dataType, size, format, mappToSystem) {
     function handleAdd(){
 
     }
+
+
+   
+
+
     return (
       <>
        
@@ -46,65 +55,90 @@ function createData(sq,fieldName, dataType, size, format, mappToSystem) {
       
         <div className={classes.demo}>
           <List dense={false}  style={{width:"100%"}}>
-           
-
-            {props.items.map(row => (
-           < div key={row.fieldName}>
-           <ListItem  key={row.fieldName}>
-           
-                  <ListItemAvatar >
-                  <Avatar className={classes.avatar}>{row.sequenceNum}</Avatar>
-                  
-                </ListItemAvatar>
-                <Grid container spacing={1}>
-              
-                <Grid item style={{flexGrow:1}}>
-                <Grid container direction="row"  spacing={1}>
-                <Grid item>
-           
-                <Typography noWrap className={classes.subTitle}> {row.fieldName}</Typography>
-           
-                  
-                   </Grid>
-                      
-
-                      <Grid container spacing={1}>
-                      <Grid item>
-                      <Typography  variant="caption" color="primary"  >Data Type: {row.dataType.desc}</Typography>
-                      </Grid>
-                      <Grid item>
-                      <Typography  variant="caption"   color="primary">Size: {row.size}</Typography>
-                      </Grid>
-                      {/* <Grid item>
-                      <Typography  variant="caption"   color="primary">Format: {row.format}</Typography>
-                      </Grid> */}
-                      
-                     
-                      
-                    </Grid>
-                    
-              </Grid>
-            
-          </Grid>
-          <Grid item>
-                <IconButton  size="small"><Edit color="primary"/></IconButton>
-                <IconButton size="small" onClick={()=>handleClickOpen()}><Clear color="error"/></IconButton>
-                
-              </Grid>
-          </Grid>
-          
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              </div>
-         ))}
+          <SortableList items={props.items} onChange={props.onChange} handleClickOpen={handleClickOpen}/>
           </List>
         </div>
+   
       </Grid>
       </Grid>
+      
       <AlertDialog open={open} handleClose={handleClose} />
       </>
     );
   }
   
+
+
+  const SortableList = ({ items, onChange,handleClickOpen }) => {
+    const classes = useStyles();
+    let sortable = null; // sortable instance
+    
+    const listItems = items.map(item => (
+            <ListItem id={item.id}  key={item.id} data-id={item.sequenceNum}>
+                
+            <ListItemAvatar >
+            <Avatar className={classes.avatar}>{item.sequenceNum}</Avatar>
+            
+          </ListItemAvatar>
+          <Grid container spacing={1}>
+        
+          <Grid item style={{flexGrow:1}}>
+          <Grid container direction="row"  spacing={1}>
+          <Grid item>
+
+          <Typography noWrap className={classes.subTitle}> {item.fieldName}</Typography>
+
+            
+            </Grid>
+                
+
+                <Grid container spacing={1}>
+                <Grid item>
+                <Typography  variant="caption" color="primary"  >Data Type: {item.dataType.desc}</Typography>
+                </Grid>
+                <Grid item>
+                <Typography  variant="caption"   color="primary">Size: {item.size}</Typography>
+                </Grid>
+                {/* <Grid item>
+                <Typography  variant="caption"   color="primary">Format: {item.format}</Typography>
+                </Grid> */}
+                
+              
+                
+              </Grid>
+              
+        </Grid>
+
+      </Grid>
+      <Grid item>
+          <IconButton  size="small"><Edit color="primary"/></IconButton>
+          <IconButton size="small" onClick={handleClickOpen}><Clear color="error"/></IconButton>
+          
+        </Grid>
+      </Grid>
+
+        </ListItem>
+));
+
+    return (
+        <div>
+           
+           
+            <ReactSortable  
+                    // tag="ul" // Defaults to "div"
+                    onChange={(order, sortable, evt) => {
+                        // console.log(evt);
+                        // console.log(sortable);
+                         
+                        //  this.setState({ items: newOrder(order,this.state.items) });
+                        onChange(order,evt.oldIndex+1,evt.newIndex+1);
+                        
+                    }}
+                >
+                     {listItems}
+                </ReactSortable >
+        </div>
+    );
+};
   
 export default ConfigList;
